e43ce9df4b1927686c6ac765fb67114ebd7c6708
Gaantz <cristian.palomino.rivera@hotmail.com>
2015-05-02 01:35:30 -0500

    Cadenas primera parte

--- Basico/cadenas.py
+++ Basico/cadenas.py
@@ -1,1 +1,12 @@
 #Cadenas en Python
+
+cadena_a = "Simple String"
+print cadena_a
+
+#Imprimir cadenas largas
+cadena_larga =  """Its lorem ipsum Its lorem ipsum Its lorem ipsum
+Its lorem ipsum Its lorem ipsum Its lorem ipsum
+Its lorem ipsum Its lorem ipsum Its lorem ipsum
+Its lorem ipsum Its lorem ipsum Its lorem ipsum
+Its lorem ipsum Its lorem ipsum Its lorem ipsum"""
+print cadena_larga
