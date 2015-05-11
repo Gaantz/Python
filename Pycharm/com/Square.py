5b6bc0a5f39109c26acb58e346fd8abb1154d87d
Gaantz <cristian.palomino.rivera@hotmail.com>
2015-05-11 11:09:46 -0500

    Integracion con pycharm

---
+++ Pycharm/com/Square.py
@@ -0,0 +1,13 @@
+__author__ = 'Gaantz'
+
+
+class Square:
+
+    x = 0
+    y = 0
+
+    def __init__(self):
+        pass
+
+    def area(self):
+        return self.x * self.y
---
+++ Pycharm/com/main.py
@@ -0,0 +1,9 @@
+from com.Square import Square
+
+__author__ = 'Gaantz'
+
+square = Square()
+square.x = 5
+square.y = 5
+
+print 'El area es: ', square.area()
